783eaa85618e0c3f767217532526393e19b5332f
Tim <tmc1618@gmail.com>
2016-09-13 10:30:39 -0500

    initial upload

--- app/components/BudgetItem.jsx
+++ app/components/BudgetItem.jsx
@@ -1,13 +1,16 @@
 var React = require('react');
 
 var BudgetItem = React.createClass({
+	clickHandler: function(amount) {
+		console.log(amount);
+	},
+
   render: function () {
     var {description, amount} = this.props;
-    console.log(this.props);
     return (
         <tr>
           <td>{description}</td>
-          <td className="amount">${amount}</td>
+          <td className="amount" onClick={this.clickHandler}>${amount}</td>
         </tr>
     )
   }
